dce6da1db672fba2fbb2df253b6c6884c2d778d9
indhira15 <indhira.ramirez@utec.edu.pe>
2020-06-28 23:25:31 -0500

    new

--- backend/pc4.py
+++ backend/pc4.py
@@ -1,50 +1,25 @@
 import pandas as pd
 import numpy as np
+import time
 
 encodings = pd.read_csv("encodings/complete.txt", header=None)
 
-# iloc para traer filas y columnas, like an matrix for data
 data = encodings.iloc[ : ,1:129]
 data.head(5933)
 
 # definir querys
-N = [5000,180,3258]
+N = [3000]
 Qx = []
 for i in N:
   Qx.append(data.iloc[i])
 
 dataT = data.drop(N, axis=0)
 
-#definir la medida de distancia euclediana
+# Definir la medida de distancia euclediana y manhattan
 DE_l2 = lambda x,y : sum((x-y)**2)**0.5
 DM_l1 = lambda x,y : sum(abs(x-y))
 
 """Algoritmo de busqueda KNN"""
-
-def knnSearch(data, Q, k):
-  result = []
-  for index,row in data.iterrows():
-      d = DE_l2(Q, row)
-      result.append((index,d))
-  result.sort(key = lambda tup: tup[1])
-  return result[:k]
-
-#evaluar algortmo
-for n_query in range(len(N)):
-  for k in [2,4,8,16,32]:
-    p1 = knnSearch(dataT, Qx[n_query],k)
-    target = encodings.iloc[N[n_query],129]
-    count = 0
-    for i,d in p1:
-      if target == encodings.iloc[i,129]:
-        count +=1
-    print("Elementos recuperados", len(p1))
-    print("Presicion: ", count/len(p1))
-    print(encodings.iloc[[x for x,y in p1], 129])
-    print(target)
-
-data.head(5933)
-
 import heapq
 
 def knnSearchHeap(data, Q, k):
@@ -58,19 +33,78 @@ def knnSearchHeap(data, Q, k):
     result = list(reversed(result))
     return result
 
-# evaluar algoritmo
-for n_query in range(len(N)):
-  for k in [2,4,8,16,32]:
-      result = knnSearchHeap(dataT, Qx[n_query], k)
-      target = encodings.iloc[N[n_query], 129]
-      cont = 0
-      for d,i  in result:
-          if target == encodings.iloc[i, 129]:
-              cont += 1
-      print("Precision:", cont/len(result)) 
-      print(encodings.iloc[[x for y,x in result], 129])
+def knnSearchHeap_MD(data, Q, k):
+    result = []
+    for index, row in data.iterrows():
+      d = DM_l1(Q, row)   
+      heapq.heappush(result, (-d, index))
+    while len(result) > k:
+      heapq.heappop(result)    
+    result = [ heapq.heappop(result) for _ in range(len(result))]
+    result = list(reversed(result))
+    return result
 
-print(encodings)
+def knnSearch(data, Q180180, k):
+  result = []
+  for index,row in data.iterrows():
+      d = DE_l2(Q, row)
+      result.append((index,d))
+  result.sort(key = lambda tup: tup[1])
+  return result[:k]
+
+#evaluar knnSearch
+""""
+for n_query in range(len(N)):
+  for k in [4,8,16]:
+    p1 = knnSearch(dataT, Qx[n_query],k)
+    target = encodings.iloc[N[n_query],129]
+    count = 0
+    for i,d in p1:
+      if target == encodings.iloc[i,129]:
+        count +=1
+    print("Elementos recuperados", len(p1))
+    print("Presicion: ", count/len(p1))
+    print(encodings.iloc[[x for x,y in p1], 129])
+    print(target)
+"""
+
+# evaluar KnnSearchHeap
+print("ED")
+for n_query in range(len(N)):
+  for k in [4,8,16]:
+    print("k: ",k)
+    result = knnSearchHeap(dataT, Qx[n_query], k)
+    target = encodings.iloc[N[n_query], 129]
+    cont = 0
+    for d,i  in result:
+      if target == encodings.iloc[i, 129]:
+          cont += 1
+    print("Precision:", cont/len(result)) 
+    #print(encodings.iloc[[x for y,x in result], 129])
+
+#evaluar knnHeap_MD
+print("MD")
+for n_query in range(len(N)):
+  for k in [4,8,16]:
+    print("k: ",k)
+    result = knnSearchHeap_MD(dataT, Qx[n_query], k)
+    target = encodings.iloc[N[n_query], 129]
+    cont = 0
+    for d,i  in result:
+      if target == encodings.iloc[i, 129]:
+          cont += 1
+    print("Precision:", cont/len(result)) 
+
+#evaluar tiempos    
+size_data = [100,200,400,800,1600,3200,6400,12800]
+
+for i in size_data:
+  data_aux = dataT.iloc[:i,:]
+  st = time.time()
+  p1 = knnSearchHeap(data_aux,Qx[0],4)
+  ft = time.time()
+  print(i,": ",ft-st)
+
 
 from rtree import index
 from sklearn.decomposition import PCA
