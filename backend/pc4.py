3759bae05c27691f24fc6054a44fc2f46d92cd42
indhira15 <indhira.ramirez@utec.edu.pe>
2020-06-28 20:48:55 -0500

    new

---
+++ backend/pc4.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+
+from google.colab import files
+files.upload()
+
+encodings = pd.read_csv("0_478.txt", header=None)
+
+# iloc para traer filas y columnas, like an matrix for data
+data = encodings.iloc[ : ,1:129]
+data.head(5933)
+
+# definir querys
+N = [5000,180,3258]
+Qx = []
+for i in N:
+  Qx.append(data.iloc[i])
+
+dataT = data.drop(N, axis=0)
+
+#definir la medida de distancia euclediana
+DE_l2 = lambda x,y : sum((x-y)**2)**0.5
+DM_l1 = lambda x,y : sum(abs(x-y))
+
+"""Algoritmo de busqueda KNN"""
+
+def knnSearch(data, Q, k):
+  result = []
+  for index,row in data.iterrows():
+      d = DE_l2(Q, row)
+      result.append((index,d))
+  result.sort(key = lambda tup: tup[1])
+  return result[:k]
+
+#evaluar algortmo
+for n_query in range(len(N)):
+  for k in [2,4,8,16,32]:
+    p1 = knnSearch(dataT, Qx[n_query],k)
+    target = encodings.iloc[N[n_query],129]
+    count = 0
+    for i,d in p1:
+      if target == encodings.iloc[i,129]:
+        count +=1
+    print("Elementos recuperados", len(p1))
+    print("Presicion: ", count/len(p1))
+    print(encodings.iloc[[x for x,y in p1], 129])
+    print(target)
+
+data.head(5933)
+
+import heapq
+
+def knnSearchHeap(data, Q, k):
+    result = []
+    for index, row in data.iterrows():
+      d = DE_l2(Q, row)   
+      heapq.heappush(result, (-d, index))
+    while len(result) > k:
+      heapq.heappop(result)    
+    result = [ heapq.heappop(result) for _ in range(len(result))]
+    result = list(reversed(result))
+    return result
+
+# evaluar algoritmo
+for n_query in range(len(N)):
+  for k in [2,4,8,16,32]:
+      result = knnSearchHeap(dataT, Qx[n_query], k)
+      target = encodings.iloc[N[n_query], 129]
+      cont = 0
+      for d,i  in result:
+          if target == encodings.iloc[i, 129]:
+              cont += 1
+      print("Precision:", cont/len(result)) 
+      print(encodings.iloc[[x for y,x in result], 129])
+
+print(encodings)
